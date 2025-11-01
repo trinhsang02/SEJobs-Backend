e1ca4e6a88397ca196d1b7233435b66b642a5d62
DDP16 <phat16122004@gmail.com>
2025-11-02 01:58:02 +0700

    Update createApp.ts

--- src/createApp.ts
+++ src/createApp.ts
@@ -13,7 +13,9 @@ export const createApp = () => {
   const app = express();
 
   // Middlewares
-  app.use(cors());
+  app.use(cors({
+    exposedHeaders: ["Authorization"],
+  }));
   app.use(express.json({ limit: "10mb" }));
   app.use(express.urlencoded({ extended: true }));
 
